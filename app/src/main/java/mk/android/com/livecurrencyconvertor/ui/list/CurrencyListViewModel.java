58d157929e6bfb75b1adc5f7bc35a96b84960bae
mayurik18 <mayuri.k18@gmail.com>
2018-12-13 01:23:11 +0530

    update Currency every 1 second

--- app/src/main/java/mk/android/com/livecurrencyconvertor/ui/list/CurrencyListViewModel.java
+++ app/src/main/java/mk/android/com/livecurrencyconvertor/ui/list/CurrencyListViewModel.java
@@ -5,13 +5,18 @@ import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.observers.DisposableSingleObserver;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subscribers.DisposableSubscriber;
 import mk.android.com.livecurrencyconvertor.data.model.Currency;
 import mk.android.com.livecurrencyconvertor.data.rest.CurrencyRepository;
 import mk.android.com.livecurrencyconvertor.util.Mappers;
@@ -19,7 +24,7 @@ import mk.android.com.livecurrencyconvertor.util.Mappers;
 public class CurrencyListViewModel extends ViewModel {
 
     private final CurrencyRepository currencyRepository;
-    private CompositeDisposable disposable;
+    DisposableSubscriber<List<Currency>> disposableSubscriber;
 
     private final MutableLiveData<List<Currency>> repos = new MutableLiveData<>();
     private final MutableLiveData<Boolean> repoLoadError = new MutableLiveData<>();
@@ -28,8 +33,7 @@ public class CurrencyListViewModel extends ViewModel {
     @Inject
     public CurrencyListViewModel(CurrencyRepository currencyRepository) {
         this.currencyRepository = currencyRepository;
-        disposable = new CompositeDisposable();
-        fetchRepos();
+        fetchCurrencies();
     }
 
     LiveData<List<Currency>> getRepos() {
@@ -41,36 +45,48 @@ public class CurrencyListViewModel extends ViewModel {
     LiveData<Boolean> getLoading() {
         return loading;
     }
+    private  PublishSubject<Long> publishSubject = PublishSubject.create();
 
-    private void fetchRepos() {
+    private void fetchCurrencies() {
         loading.setValue(true);
-        disposable.add(
-            currencyRepository.getCurrency()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .map(Mappers::mapRemoteToLocal)
-                .subscribeWith(new DisposableSingleObserver<List<Currency>>() {
-                    @Override
-                    public void onSuccess(List<Currency> value) {
-                        repoLoadError.setValue(false);
-                        repos.setValue(value);
-                        loading.setValue(false);
-                    }
+            Scheduler scheduler = Schedulers.from(Executors.newSingleThreadExecutor());
+         disposableSubscriber = new DisposableSubscriber<List<Currency>>() {
+            @Override
+            public void onNext(List<Currency> value) {
+                repoLoadError.setValue(false);
+                repos.setValue(value);
+                loading.setValue(false);
+            }
 
-                    @Override
-                    public void onError(Throwable e) {
-                        repoLoadError.setValue(true);
-                        loading.setValue(false);
-                    }
-                }));
-    }
+            @Override
+            public void onError(Throwable t) {
+                repoLoadError.setValue(true);
+                loading.setValue(false);
+            }
+
+            @Override
+            public void onComplete() {
+            }
+        };
+        Flowable.interval(1, TimeUnit.SECONDS)
+                    .subscribeOn(scheduler)
+                    .mergeWith(publishSubject.toFlowable(BackpressureStrategy.DROP))
+                    .onBackpressureDrop()
+                    .concatMap(n -> {
+                           return currencyRepository.getCurrency()
+                                    .map(Mappers::mapRemoteToLocal)
+                                    .toFlowable();
+                                   })
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(disposableSubscriber);
+        }
 
     @Override
     protected void onCleared() {
         super.onCleared();
-        if (disposable != null) {
-            disposable.clear();
-            disposable = null;
+
+        if (disposableSubscriber != null) {
+            disposableSubscriber.dispose();
         }
     }
 }
